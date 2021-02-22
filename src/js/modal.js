bb6dc4ec24162835e16d5ff87546a6d2c9b87503
Julia <kleimenova.j@fmail.com>
2021-02-22 16:55:08 +0200

    latest

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,30 +1,13 @@
 ;(() => {
-  const refs = {
-    openContactsBtn: document.querySelector('[data-contacts-open]'),
-    modalContacts: document.querySelector('#modalContacts'),
-    openModalBtn: document.querySelectorAll('.btn'),
-    modalRequest: document.querySelector('#modalRequest'),
-    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
-  }
+  const openMenuBtn = document.querySelector('[data-modal-btn]')
+  const mobileMenu = document.querySelector('[data-menu]')
 
-  refs.openModalBtn.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-      modalRequest.classList.add('is-open')
-    })
+  openMenuBtn.addEventListener('click', () => {
+    const expanded = openMenuBtn.getAttribute('aria-expanded') === 'true' || false
+
+    openMenuBtn.classList.toggle('is-active')
+    openMenuBtn.setAttribute('aria-expanded', !expanded)
+
+    mobileMenu.classList.toggle('is-open')
   })
-
-  refs.openContactsBtn.addEventListener('click', function() {
-    modalContacts.classList.add('is-open')
-  })
-
-  refs.closeModalBtn.forEach(function(btnClose) {
-    btnClose.addEventListener('click', function(e) {
-      closeModal()
-    })
-  })
-
-  function closeModal() {
-    modalRequest.classList.remove('is-open')
-    modalContacts.classList.remove('is-open')
-  }
 })()
