d8506226fba2fc37a9c65f6e0309c9b4ab9606da
Julia <kleimenova.j@fmail.com>
2021-02-23 22:46:15 +0200

    smooth scroll, leeloo, mobile menu close

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,8 +2,19 @@
   const openMenuBtn = document.querySelector('[data-menu-btn]')
   const mobileMenu = document.querySelector('[data-menu]')
   const body = document.querySelector('.body')
+  const menuLink = document.querySelectorAll('.mobile-menu .menu-link')
 
   openMenuBtn.addEventListener('click', () => {
+    toggleMenu()
+  })
+
+  menuLink.forEach(function(link) {
+    link.addEventListener('click', () => {
+      toggleMenu()
+    })
+  })
+
+  function toggleMenu() {
     const expanded = openMenuBtn.getAttribute('aria-expanded') === 'true' || false
 
     openMenuBtn.classList.toggle('is-active')
@@ -11,5 +22,5 @@
 
     mobileMenu.classList.toggle('is-open')
     body.classList.toggle('scroll-hidden')
-  })
+  }
 })()
