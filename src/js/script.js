3bc25d9daa5380d341c654e2d7a61cd353afaab3
Andrii Kochmaruk <luckyanry@gmail.com>
2021-02-23 22:07:30 +0100

    added couner animation effect into what-is section

--- src/js/script.js
+++ src/js/script.js
@@ -43,3 +43,24 @@ $('.js-btn--fly').click(function () {
     return text === 'Забронировано' ? 'Забронировать место' : 'Забронировано'
   })
 })
+
+/* Counter effect for what-is section */
+const counters = document.querySelectorAll('[data-target]')
+
+counters.forEach(counter => {
+  counter.innerHTML = '0'
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target')
+    const inc = +counter.innerText
+
+    if (inc < target) {
+      counter.innerText = inc + 1
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+
+  updateCounter()
+})
