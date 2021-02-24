9986c56723e781b6772c1a7c7a7770b306d9b2e3
Julia <kleimenova.j@fmail.com>
2021-02-24 19:40:13 +0200

    Updates

--- modal.4331011c.js
+++ modal.4331011c.js
@@ -135,8 +135,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    modal.classList.toggle('is-hidden');
-    body.classList.toggle('scroll-hidden');
+    modal.classList.toggle('is-hidden'); // body.classList.toggle('scroll-hidden')
   }
 })();
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -167,7 +166,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60939" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52707" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
